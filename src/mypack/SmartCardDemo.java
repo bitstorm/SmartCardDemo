b9dbb06770a70b17dd23ad2719f1ddb6450775f6
Andrea Del Bene <adelbene@apache.org>
2016-08-20 20:25:19 +0200

    Pulizia codice

--- src/mypack/SmartCardDemo.java
+++ src/mypack/SmartCardDemo.java
@@ -38,13 +38,6 @@ public class SmartCardDemo {
 
 		CardChannel channel = card.getBasicChannel();
 
-		// Send Select Applet command
-		// byte[] aid = {(byte)0xA0, 0x00, 0x00, 0x00, 0x62, 0x03, 0x01, 0x0C,
-		// 0x06, 0x01};
-		// ResponseAPDU answer = channel.transmit(new CommandAPDU(0x00, 0xA4,
-		// 0x04, 0x00, aid));
-		// System.out.println("answer: " + answer.toString());
-
 		ResponseAPDU answer = channel.transmit(new CommandAPDU(CMD_SELECT_MF));
 		System.out.println("answer: " + answer.toString());
 
@@ -56,12 +49,7 @@ public class SmartCardDemo {
 
 		answer = channel.transmit(new CommandAPDU(CMD_READ_BINARY));
 		System.out.println("answer: " + new String(answer.getData()));
-		// Send test command
-		// answer = channel.transmit(new CommandAPDU(0x00, 0x00, 0x00, 0x00));
-		// System.out.println("answer: " + answer.toString());
-		// byte r[] = answer.getData();
-		// for (int i=0; i<r.length; i++)
-		// System.out.print((char)r[i]);
+	
 		card.disconnect(false);
 	}
 }
